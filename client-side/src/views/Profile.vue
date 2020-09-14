ba553befae27f240d634282ab330cb859560fe79
Kirill Chernyshov <k.c.ru@mail.ru>
2020-09-14 10:41:25 +0300

    [fix] Кнопка в профиле

--- client-side/src/views/Profile.vue
+++ client-side/src/views/Profile.vue
@@ -9,8 +9,8 @@
             </div>
         </div>
         <b-table v-if="tickets.length" sticky-header class="align-left" striped hover :items="tickets" :fields="fields">
-            <template v-if="row.item.is_booked" v-slot:cell(actions)="row">
-                <b-button size="sm" @click="cancelReservation(row.item.ticket_id)" class="mr-2">
+            <template v-slot:cell(actions)="row">
+                <b-button v-if="row.item.is_booked" size="sm" @click="cancelReservation(row.item.ticket_id)" class="mr-2">
                 &#10008;
                 </b-button>
             </template>
